5c18247954abba37fd507dd2600aa331ba839ac5
Sergey Murintsov <sm-mars@mail.ru>
2024-08-11 22:30:56 +0300

    Шестое задание выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(value) {
+  // const result = Number.parseFloat(value);
+  if (isNaN(value)) {
+    throw new Error("Невалидное значение");
+  }
+  return Number.parseFloat(value);
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+  get area() {
+    const halfOfPerimeter = this.perimeter / 2;
+    return Number(
+      Math.sqrt(
+        halfOfPerimeter *
+          (halfOfPerimeter - this.a) *
+          (halfOfPerimeter - this.b) *
+          (halfOfPerimeter - this.c)
+      ).toFixed(3)
+    );
+  }
+}
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
